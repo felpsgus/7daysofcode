b6a574ca14d975c8575c28c7b05bd351ce8f9bdd
felpsgus <felipeaugustolopesveras@gmail.com>
2022-04-14 17:27:41 -0300

    alteração desafio 7

--- desafio 7/script.js
+++ desafio 7/script.js
@@ -1,21 +1,42 @@
-function insert(num) {
-    document.querySelector("#resultado").innerHTML += num
-}
+const botoes = document.querySelector(".botoes")
 
-function clean() {
-    document.querySelector("#resultado").innerHTML = ""
-}
+botoes.addEventListener("click", geral)
 
-function back() {
-    var resultado = document.querySelector("#resultado").innerHTML
-    document.querySelector("#resultado").innerHTML = resultado.substring(0, resultado.length -1)
-}
+function geral(event) {
 
-function calcular() {
-    var resultado = document.querySelector("#resultado").innerHTML
-    if (resultado) {
-        document.querySelector("#resultado").innerHTML = eval(resultado)
-    } else {
-        alert("Nada para calcular")
+    const elemento  = event.target
+
+    var valor1      = document.querySelector(".valor1").value
+    var valor2      = document.querySelector(".valor2").value
+
+
+    if (valor1 == "" || valor2 == "") {
+        alert("adicione os valores antes de selecionar as operações")
+
+    } else if (elemento.id == "soma") {
+        document.querySelector(".resultado").innerText = Number(valor1) + Number(valor2)
+
+    } else if (elemento.id == "subtrair") {
+        document.querySelector(".resultado").innerText = Number(valor1) - Number(valor2)
+
+    } else if (elemento.id == "multiplicar") {
+        document.querySelector(".resultado").innerText = Number(valor1) * Number(valor2)
+
+    } else if (elemento.id == "dividir") {
+        document.querySelector(".resultado").innerText = Number(valor1) / Number(valor2)
+
     }
+}
+
+function limpar() {
+    document.querySelector(".resultado").innerText = ""
+    document.querySelector(".valor1").value = ""
+    document.querySelector(".valor2").value = ""
+
+}
+
+function sair() {
+    document.querySelector(".corpo").style.display = "none"
+    document.querySelector(".despedida").style.display = "block"
+
 }
